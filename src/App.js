3082957fe608bd46b3a220dc264de0d8a151c372
JC <jctrilamanila@gmail.com>
2021-03-06 15:57:31 -0500

    created todo app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  );
+}
+
+export default App;
