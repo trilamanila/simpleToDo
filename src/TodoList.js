ae246d26bc06de7a73fa16d5a789335918552182
JC <jctrilamanila@gmail.com>
2021-03-07 23:00:35 -0500

    made responsive clear todo button and number of current todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, toggleTodo }) {
   return todos.map(todo => {
-    return <Todo todo={todo} />;
+    return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />;
   });
 }
 
